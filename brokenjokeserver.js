45de7ec702d9217d7b42a472176e5628e6bb5f6a
idonteatenoughpies <75098908+idonteatenoughpies@users.noreply.github.com>
2021-06-10 17:01:37 +0100

    broke it

--- brokenjokeserver.js
+++ brokenjokeserver.js
@@ -4,5 +4,5 @@ var knockknock = require ('knock-knock-jokes');
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type':'text/html'});
     var randomJoke = knockknock()
-    res.end(randomJoke);
+    res.end(randomJoke)
 }).listen(8080);
