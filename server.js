f3abdcff4a2656a09796132a6e1ba7236cccd236
idonteatenoughpies <75098908+idonteatenoughpies@users.noreply.github.com>
2021-06-14 10:27:15 +0100

    added joke route

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req,res){
+const http = require ('http');
+var knockknock = require ('knock-knock-jokes');
+
+
+app.get('/', (req, res) => {
     res.send("Hello World! by express");
 });
 
-app.get('/test', function (req,res){
+app.get('/test', (req, res) =>{
     res.send("This is route 2");
+});
+
+app.get('/joke', (req, res) =>{
+    res.writeHead(200, {'Content-Type':'text/html'});
+    const randomJoke = knockknock()
+    res.end(randomJoke);
 });
 
 app.listen(8080);
