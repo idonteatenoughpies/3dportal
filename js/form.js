6569c6fb4a3a30f72cc73da7ee418cccf96f87bc
idonteatenoughpies <75098908+idonteatenoughpies@users.noreply.github.com>
2021-06-14 12:38:18 +0100

    moved script to separate file

---
+++ js/form.js
@@ -0,0 +1,16 @@
+$('#submit').click(handleClick);
+
+      function handleClick() {
+          const nametosend = $('#name').val();
+          const questtosend = $('#quest').val();
+
+          const url = '/getform?name='+nametosend+"&quest="+questtosend;
+
+          $.ajax(
+              {url: url, 
+            success: (result) => {
+                $("#result").html(result);
+                }
+              });
+              return false;
+      }
