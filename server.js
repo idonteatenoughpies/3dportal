f37a2c2481b828e75649bd65f0e8d64e98e5e7f4
idonteatenoughpies <75098908+idonteatenoughpies@users.noreply.github.com>
2021-06-16 14:40:46 +0100

    removed a load of code to test

--- server.js
+++ server.js
@@ -18,58 +18,6 @@ app.get('/test', (req, res) => {
     res.send("This is route 2");
 });
 
-app.get('/joke', (req, res) =>{
-    res.writeHead(200, {'Content-Type':'text/html'});
-    const randomJoke = knockknock()
-    res.end(randomJoke);
-});
-
-app.get('/add', (req, res) => {
-    const x = parseInt(req.query.x);
-    const y = parseInt(req.query.y);
-    res.send("X + Y= " + (x+y));
-});
-
-app.get('/calc', (req, res) => {
-    const x = parseInt(req.query.x);
-    const y = parseInt(req.query.y);
-    var operator = (req.query.operator);
-    switch(operator){
-        case 'add': var operatorA = 'add';
-        answer = x+y;
-        break;
-        case 'sub': var operatorA = 'minus';
-        answer = x-y;
-        break;
-        case 'mul': var operatorA = 'multiplied by';
-        answer = x*y;
-        break;
-        case 'div': var operatorA ='divided by';
-        answer = x/y;
-        break;
-    }
-    res.send("X " + operatorA + " Y= " + answer );
-});
-
-app.get('/getform', (req, res) => {
-const name = req.query.name;
-const quest = req.query.quest;
-res.send('Hi ' + name + " I am sure you will " + quest);
-});
-
-app.post('/postform', (req,res) => {
-const name = req.body.name;
-const quest = req.body.quest;
-res.send('Hi ' + name + " I am sure you will " + quest);
-});
-
-app.get ('/user/:userid/books/:bookid', (req, res) =>{
-var userID = req.params.userid;
-var bookID = req.params.bookid;
-res.send ("Hi user "+ userID + ", you've checked out book number " + bookID);
-});
-
-
 app.use ((req, res, next) => {
     res.sendFile('404.html', {root : path.join(__dirname, '/public')});
 });
