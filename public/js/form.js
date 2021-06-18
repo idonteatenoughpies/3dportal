8371e5b6d257248e306bb6a938b23be7b440ca99
idonteatenoughpies <75098908+idonteatenoughpies@users.noreply.github.com>
2021-06-18 11:46:44 +0100

    tweaked

--- public/js/form.js
+++ public/js/form.js
@@ -4,13 +4,11 @@ $("#submit").click(handleClick);
           var nametosend = $('#name').val();
           var questtosend = $('#quest').val();
 
-
           var url = "/getform?"+"name="+nametosend+"&quest="+questtosend;
           
-
           $.ajax(
               {url: url,
-            success: (result) => {
+            success: function(result) {
                 $("#result").html(result);
                 }
               });
