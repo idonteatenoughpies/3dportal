03b31608e68a6d082461107c48077a4357ba299b
idonteatenoughpies <75098908+idonteatenoughpies@users.noreply.github.com>
2021-06-18 11:35:11 +0100

    added a home page with link

--- server.js
+++ server.js
@@ -15,10 +15,6 @@ app.get('/', (req, res) => {
     res.send("Hello World! by express");
 });
 
-app.get('/test', (req, res) => {
-    res.send("This is route 2");
-});
-
 app.get('/joke', (req, res) =>{
     res.writeHead(200, {'Content-Type':'text/html'});
     const randomJoke = knockknock()
