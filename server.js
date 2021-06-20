68a98da8c306bca2b29b817847aee5e03b5c6a9a
idonteatenoughpies <75098908+idonteatenoughpies@users.noreply.github.com>
2021-06-20 18:39:27 +0100

    as

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-//const router = express.Router();
 
 const path = __dirname + '/public/';
 const port = 80;
@@ -12,23 +11,12 @@ app.use (function (req,res,next) {
 
 app.set('view engine', 'ejs');
 app.use(express.static(path));
-//app.use('/', router);
+
 app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
     res.render('index');
 });
-
-/*router.get('/hello', function(req,res){
-    res.sendFile(path + 'hello.html');
-  });
-
-  router.get('/testing', function(req,res) {
-    res.send('testing works');
-});
-*/
-
-
 
 app.listen(port, function () {console.log('Example app is listening on port:' + port);
 })
