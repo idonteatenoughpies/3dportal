7aa02300ee14d3040cadbd7af543caed344c4b0f
idonteatenoughpies <75098908+idonteatenoughpies@users.noreply.github.com>
2021-06-21 20:21:40 +0100

    added image check

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ return res.json(files);
 
 
 // GET /files/:filename
-// Displays all files in json
+// Displays single file object
 app.get('/files/:filename', (req, res) => {
     gfs.files.findOne({filename: req.params.filename}, (err, file) => {
 // Check if files
@@ -111,6 +111,27 @@ if(!file || file.length === 0) {
 }
 // File exists
 return res.json(file);
+    });
+});
+
+// GET /image/:filename
+// Displays image
+app.get('/image/:filename', (req, res) => {
+    gfs.files.findOne({filename: req.params.filename}, (err, file) => {
+// Check if files
+if(!file || file.length === 0) {
+    return res.status(404).json({
+        err: 'No file exist'
+    });
+}
+// Check if image
+if(file.contentType === 'image/jpeg' || file.contentType === 'img/png'){
+// Read output to browser
+const readstream = gfs.createReadStream(file.filename);
+readstream.pipe(res);
+} else {
+    res.status(404).json({err: 'Not an Image'})
+}
     });
 });
     
