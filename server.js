b0dd0404f0afc2407d1bd445366602cfc15552e8
idonteatenoughpies <75098908+idonteatenoughpies@users.noreply.github.com>
2021-06-21 16:05:27 +0100

    added upload

--- server.js
+++ server.js
@@ -1,7 +1,52 @@
 const express = require('express');
-const app = express();
 const db = require('./db');
+const path = require('path');
+const crypto = require ('crypto');
+const mongoose = require ('mongoose');
+const multer = require ('multer');
+const GridFSStorage = require ('multer-gridfs-storage');
+const Grid = require ('gridfs-stream');
+const methodOverride = require ('method-override');
+const bodyParser = require ('body-parser');
+const { connect } = require('http2');
 
+const app = express();
+
+//Middleware
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs');
+app.use(express.static(path));
+app.use(express.urlencoded({extended:true}));
+
+//Initialise the stream
+let gfs;
+conn.once('open', () => {
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+})
+
+//Create storage engine
+const storage = new GridFsStorage({
+    url: mongoURL,
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) {
+            return reject(err);
+          }
+          const filename = buf.toString('hex') + path.extname(file.originalname);
+          const fileInfo = {
+            filename: filename,
+            bucketName: 'uploads'
+          };
+          resolve(fileInfo);
+        });
+      });
+    }
+  });
+  const upload = multer({ storage });
+ 
 const path = __dirname + '/public/';
 const port = 80;
 
@@ -10,10 +55,6 @@ app.use (function (req,res,next) {
     next();
 });
 
-app.set('view engine', 'ejs');
-app.use(express.static(path));
-
-app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -25,6 +66,10 @@ app.get('/registration', (req, res) => {
 
 app.get('/application', (req, res) => {
     res.render('application');
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.json({file: req.file});
 });
 
 app.listen(port,  () => console.log(`App is listening on port: ${port}`));
