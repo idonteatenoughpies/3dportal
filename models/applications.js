045cc21982351b3336ab38c5b949dc182e7dc7d6
idonteatenoughpies <75098908+idonteatenoughpies@users.noreply.github.com>
2021-06-21 14:22:28 +0100

    updated package.json

---
+++ models/applications.js
@@ -0,0 +1,15 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const Application = new Schema ({
+applicationName:
+applicationID:
+applicationDescription:
+location:
+submitterID:
+submitterName:
+model:
+modelPath:
+
+
+});
