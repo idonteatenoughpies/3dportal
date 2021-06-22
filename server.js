23acf44cfcf79af0944a69a884a02a6b8f0d2659
idonteatenoughpies <75098908+idonteatenoughpies@users.noreply.github.com>
2021-06-22 11:52:49 +0100

    as

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
 app.use(express.urlencoded({extended:true}));
-app.use(favicon("/images/favicon)"));
+app.use(favicon("/images/favicon/favicon.ico)"));
 
 //Initialise the stream
 let gfs;
