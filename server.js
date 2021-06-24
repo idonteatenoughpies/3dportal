eb820a98c3cf41341e8f55122b06de4fb982389b
idonteatenoughpies <75098908+idonteatenoughpies@users.noreply.github.com>
2021-06-24 16:21:04 +0100

    asd

--- server.js
+++ server.js
@@ -124,6 +124,21 @@ app.get('/dashboard', checkToken, (req,res) => {
         })
     });
 
+    //Check to make sure header is not undefined, if so, return Forbidden (403)
+const checkToken = (req, res, next) => {
+    const header = req.headers['authorization'];
+
+    if(typeof header !== 'undefined') {
+        const bearer = header.split(' ');
+        const token = bearer[1];
+
+        req.token = token;
+        next();
+    } else {
+        //If header is undefined return Forbidden (403)
+        res.sendStatus(403)
+    }
+}
 
 app.get('/application', (req, res) => {
     gfs.files.find().toArray((err, files) => {
