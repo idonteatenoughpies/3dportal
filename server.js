7e8c6d05db7a3e79ab368ddf4f65da7c14152930
idonteatenoughpies <75098908+idonteatenoughpies@users.noreply.github.com>
2021-06-24 19:59:23 +0100

    s

--- server.js
+++ server.js
@@ -241,7 +241,7 @@ app.post('/processlogin', (req, res) => {
         //if there is no result, redirect the user back to the login system as that username must not exist
         if(!result){res.send('no result');return}//{res.redirect('/login');return}
         //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the indexs
-       if( bcrypt.compare(password, result.login.password)){res.send("password check passed");return}//{ req.session.loggedin = true; res.redirect('/dashboard') }
+       if( bcrypt.compare(password, result.password)){res.send("password check passed");return}//{ req.session.loggedin = true; res.redirect('/dashboard') }
         //otherwise send them back to login
         //else{res.redirect('/login')}
       });
