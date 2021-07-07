f14e6d6b549c22ed9bc009016117409ee8e96ee7
idonteatenoughpies <75098908+idonteatenoughpies@users.noreply.github.com>
2021-07-07 19:54:28 +0100

    amend mongo string

--- app.js
+++ app.js
@@ -29,9 +29,10 @@ const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 const MONGO_HOSTNAME = process.env.MONGO_HOSTNAME;
 const MONGO_PORT = process.env.MONGO_PORT;
-
 const MONGO_DB = process.env.MONGO_DB;
-mongoURL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+mongoURL = process.env.MONGOSTRING;
+//mongoURL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
 mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
   const db = mongoose.connection;
