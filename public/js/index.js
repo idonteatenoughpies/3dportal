4da928980ef2826b5ed83d2d22c726599badb161
Alastair <idonteatenoughpies@outlook.com>
2021-07-30 14:07:20 +0100

    adding pictoral menu

---
+++ public/js/index.js
@@ -0,0 +1,29 @@
+function onHoverSearch()
+{
+    $("#imageBoxSearch").attr('src', '/images/search.png');
+}
+
+function offHoverSearch()
+{
+    $("#imageBoxSearch").attr('src', '/images/search_selected.png');
+}
+
+function onHoverApply()
+{
+    $("#imageBoxApply").attr('src', '/images/apply.png');
+}
+
+function offHoverApply()
+{
+    $("#imageBoxApply").attr('src', '/images/apply_selected.png');
+}
+
+function onHoverAccount()
+{
+    $("#imageBoxAccount").attr('src', '/images/account.png');
+}
+
+function offHoverAccount()
+{
+    $("#imageBoxAccount").attr('src', '/images/account_selected.png');
+}
