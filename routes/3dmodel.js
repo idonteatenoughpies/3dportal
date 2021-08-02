ccbb193de874f160d4dcc06f4e3b617f91bd5957
Alastair <idonteatenoughpies@outlook.com>
2021-08-02 14:01:03 +0100

    added authenication to route

--- routes/3dmodel.js
+++ routes/3dmodel.js
@@ -5,13 +5,17 @@ const Model = require('../model/model')
 // define the home page route
 router.get('/', function (req, res) {
   const ref = decodeURIComponent(req.query.ref);
-  Model.find({planningID: ref}, function (err, model) {
+  Model.find({ planningID: ref }, function (err, model) {
     if (err) {
-        console.log(err);
+      console.log(err);
     } else {
-    res.render('../views/3dmodel', {model: model})
-  }
-})
+      if (!req.isAuthenticated()) {
+        res.render('../views/3dmodel', { user: undefined, model: model })
+      } else {
+        res.render('../views/3dmodel', { user: req.user, model: model })
+      }
+    }
+  })
 })
 
   module.exports = router
