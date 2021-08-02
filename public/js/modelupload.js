34034181490ac55b0431ae76200e892f4238fd33
Alastair <idonteatenoughpies@outlook.com>
2021-08-02 17:11:46 +0100

    added client side file type verification

--- public/js/modelupload.js
+++ public/js/modelupload.js
@@ -1,11 +1,12 @@
 var number = 1;
 // ----------- Code to add new upload field if OBJ file detected  ---------------
 
-let modelinput = document.getElementById('modelInput')
+let modelinput = document.getElementById('modelInput0')
 modelinput.onchange = () => {
     let selectedFile = modelinput.files[0];
 
     let ext = selectedFile.name.slice((Math.max(0, selectedFile.name.lastIndexOf(".")) || Infinity) + 1);
+    console.log(ext);
     if (ext.toLowerCase() == "obj") {
         // Container <div> where dynamic content will be placed
         var container = document.getElementById("MTLContainer");
@@ -13,12 +14,12 @@ modelinput.onchange = () => {
 
         // Create an <input> element, set its type and name attributes
         let diva = document.createElement('div');
-        diva.className= "p-2";
+        diva.className = "p-2";
 
         container.appendChild(diva);
 
         let divb = document.createElement('div');
-        divb.className="custom-file";
+        divb.className = "custom-file";
         diva.appendChild(divb);
 
 
@@ -26,7 +27,7 @@ modelinput.onchange = () => {
         modelLabel.for = "modelInput" + number;
         modelLabel.id = "MTLLabel" + number;
         divb.appendChild(modelLabel);
-        var modelLabelID = "MTLLabel"  + number;
+        var modelLabelID = "MTLLabel" + number;
         var modelLabelText = "You have uploaded an OBJ file. Please also upload an MTL file to accompany it";
 
         document.getElementById(modelLabelID).innerHTML = modelLabelText;
@@ -41,5 +42,21 @@ modelinput.onchange = () => {
         document.getElementById('count').value = number;
     }
     else { document.getElementById("MTLContainer").innerHTML = ""; }
+
+    if (
+        ext.toLowerCase().trim() != "obj" &&
+        ext.toLowerCase().trim() != "mtl" &&
+        ext.toLowerCase().trim() !== "gltf" &&
+        ext.toLowerCase().trim() != "3ds" &&
+        ext.toLowerCase().trim() != "stl" &&
+        ext.toLowerCase().trim() != "ply" &&
+        ext.toLowerCase().trim() != "glb" &&
+        ext.toLowerCase().trim() != "3dm" &&
+        ext.toLowerCase().trim() != "off" &&
+        ext.toLowerCase().trim() != "mtl"
+    ) { document.getElementById("fileWarning").innerHTML = "Unsupported file type"; }
+    else {
+        document.getElementById("fileWarning").innerHTML = "";
+    }
 
 }
